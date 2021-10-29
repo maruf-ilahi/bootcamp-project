26290c8dd2db0055d9a5198fca5938a0363eb1c7
Maruf Ilahi <72516348+maruf-ilahi@users.noreply.github.com>
2021-10-29 03:35:32 +0100

    Update person-details-joins.sql
    Update join query

--- task-7/person-details-joins.sql
+++ task-7/person-details-joins.sql
@@ -1,11 +1,14 @@
 -- Person Detail Report view using joins
 
 CREATE VIEW Person_Details AS
-    SELECT FirstName, LastName, (Line1 + ' ' + Line2 + ' ' + City) AS Address, ContactDetail
+    SELECT FirstName, LastName, (Line1 + ' ' + Line2) AS Address, City, ContactDetail AS Mobile
         FROM Person
         INNER JOIN Address A on Person.PersonId = A.PersonId
         INNER JOIN AddressDetailsType ADT on ADT.AddressDetailsTypeId = A.AddressDetailsTypeId
         INNER JOIN ContactDetail CD on Person.PersonId = CD.PersonId
         INNER JOIN ContactDetailType CDT on CDT.ContactDetailTypeId = CD.ContactDetailTypeId
+        WHERE DetailType='Phone'
+
+-- Query Data using View
 
 SELECT * FROM Person_Details
